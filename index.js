97985385b8d33f8efada9b6bfa790eb280310c6e
MarleneElla <ellamarlene54@gmail.com>
2023-09-25 23:12:03 +0300

    complete assignments

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = "bob";
+function customerName (){
+    return customerName;
+}
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+}
+
+const LeastFavoriteCustomer=""
+function changeLeastFavoriteCustomer(){
+    LeastFavoriteCustomer="unsuccessful"
+}
